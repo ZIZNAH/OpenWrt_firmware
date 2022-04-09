05f662050941314ce5fa30a4339a003ae1591893
卷毛 <47733889+zoengzihaam@users.noreply.github.com>
2022-04-09 22:53:32 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# Modify Host Name
+# sed -i '/uci commit system/i uci set system.@system[0].hostname=ZiznahWrt'  package/lean/default-settings/files/zzz-default-settings
