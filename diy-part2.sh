767a5c293379839154149704e7d670117af702c3
卷毛 <47733889+zoengzihaam@users.noreply.github.com>
2022-04-12 14:34:09 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,4 +14,11 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 # Modify Host Name
-# sed -i '/uci commit system/i uci set system.@system[0].hostname=ZiznahWrt'  package/lean/default-settings/files/zzz-default-settings
+#sed -i '/uci commit system/i uci set system.@system[0].hostname=ZiznahWrt'  package/lean/default-settings/files/zzz-default-settings
+
+#Backup OpenClash cofig
+#echo '/etc/openclash/' >> package/base-files/files/etc/sysupgrade.conf
+
+# Update Luci theme argon  
+#rm -rf package/lean/luci-theme-argon  
+#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
